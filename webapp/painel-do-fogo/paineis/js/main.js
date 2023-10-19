df18b194db53b46b7163dd24700291b0ef8017bc
Helder Vendramini <hlvprado@gmail.com>
2023-10-19 11:51:07 -0300

    [FEAT] Ajustes na exibição dos gráficos do painel

--- webapp/painel-do-fogo/paineis/js/main.js
+++ webapp/painel-do-fogo/paineis/js/main.js
@@ -1,192 +1,235 @@
 google.charts.load("current", {packages:["corechart"]});
 google.charts.setOnLoadCallback(drawChart);
 
-function drawDonutChart() {
-    var data = google.visualization.arrayToDataTable([
-        ['Task', 'Hours per Day'],
-        ['Amazônia',     1226],
-        ['Cerrado',      763],
-        ['Mata Atlântica',  379],
-        ['Caatinga', 155],
-        ['Pampa',    153]
-    ]);
-
-    var options = {
-        'width':'100%',
-        'height':'100%',
-        pieHole: 0.68,
-        backgroundColor: 'transparent',
-        legend: {position: 'labeled'},
-        chartArea:{left:'0',top:'20%',width:'100%',height:'70%'},
-        slices: [{color: '#df4226'}, {color: '#e66650'}, {color: '#e38879'}, {color: '#e3aba6'}, {color: '#e4d1d0'}],
-        pieSliceBorderColor: "transparent",
-        pieSliceText: "none",
-        tooltip: {
-            trigger: "none"
-        }
+function drawChartBioma() {
+    let total = 2759;
+    let ctx = document.getElementById('grafico-rosca').getContext('2d');
+    const data = {
+        labels: [
+          'Amazônia',
+          'Cerrado',
+          'Mata Atlântica',
+          'Caatinga',
+          'Pampa'
+        ],
+        datasets: [{
+          label: '',
+          data: [1226, 763, 379, 155, 153],
+          backgroundColor: ['#df4226', '#e66650', '#e38879', '#e3aba6', '#e4d1d0'],
+          hoverOffset: 4,
+        }]
     };
-
-    var chart = new google.visualization.PieChart(document.getElementById('grafico-rosca'));
-    chart.draw(data, options);
-}
-
-
-function drawColumnChart() {
-    var data = google.visualization.arrayToDataTable([
-        ["Estado", "Total", { role: "style" } ],
-        ["1", 19, "color: #df4226"],
-        ["2", 66, "color: #df4226"],
-        ["3", 29, "color: #df4226"],
-        ["4", 80, "color: #df4226"],
-        ["5", 103, "color: #df4226"],
-        ["6", 69, "color: #df4226"],
-        ["7", 65, "color: #df4226"],
-        ["8", 31, "color: #df4226"],
-        ["9", 45, "color: #df4226"],
-        ["10", 37, "color: #df4226"],
-        ["11", 147, "color: #df4226"],
-        ["12", 119, "color: #df4226"],
-        ["13", 74, "color: #df4226"],
-        ["14", 76, "color: #df4226"],
-        ["15", 52, "color: #df4226"],
-        ["16", 158, "color: #df4226"],
-        ["17", 111, "color: #df4226"],
-        ["18", 153, "color: #df4226"],
-        ["19", 145, "color: #df4226"],
-        ["20", 146, "color: #df4226"],
-        ["21", 165, "color: #df4226"],
-        ["22", 350, "color: #df4226"],
-        ["23", 113, "color: #df4226"],
-        ["24", 101, "color: #df4226"],
-        ["25", 107, "color: #df4226"],
-        ["26", 41, "color: #df4226"],
-        ["27", 40, "color: #df4226"],
-        ["28", 26, "color: #df4226"],
-        ["29", 15, "color: #df4226"],
-        ["30", 50, "color: #df4226"],
-        ["31", 26, "color: #df4226"]
-    ]);
-
-    var options = {
-        'width':'100%',
-        'height':'100%',
-        backgroundColor: 'transparent',
-        legend: {position: 'none'},
-        chartArea:{left:'3%',top:'20%',width:'94%',height:'70%'},
-        pieSliceText: "labeled",
-        tooltip: {
-            trigger: "none"
-        },
-        hAxis : { 
-            textStyle : {
-                fontSize: 9,
-                bold: true,
-                fontWeight: "900",
-                color: "#df4226"
-            }
-        },
-        vAxis: {
-            textStyle : {
-                fontSize: 9,
-                bold: true,
-                fontWeight: "900",
-                color: "#df4226"
-            }
-        },
-        annotations: {
-            alwaysOutside: true,
-            stemColor : 'none',
-            textStyle: {
-                fontSize: 9,
-                bold: true,
-                auraColor: 'none',
-                color: '#333333'
-            }
-        }
-    };
-
-    var view = new google.visualization.DataView(data);
-    view.setColumns([0, 1,
-                     { calc: "stringify",
-                       sourceColumn: 1,
-                       type: "string",
-                       role: "annotation" },
-                     2]);
-
-    var chart = new google.visualization.ColumnChart(document.getElementById('grafico-colunas'));
-    chart.draw(view, options);
-}
-
-function drawBarChart() {
-    var data = google.visualization.arrayToDataTable([
-        ["Estado", "Total", { role: "style" } ],
-        ["MT", 556, "color: #df4226"],
-        ["RR", 371, "color: #e66650"],
-        ["MS", 205, "color: #e38879"],
-        ["PA", 202, "color: #e38879"],
-        ["MA", 185, "color: #e38879"],
-        ["RS", 174, "color: #e38879"],
-        ["AM", 157, "color: #e38879"],
-        ["BA", 149, "color: #e3aba6"],
-        ["MG", 110, "color: #e3aba6"],
-        ["RO", 80, "color: #e3aba6"]
-      ]);
-
-      var view = new google.visualization.DataView(data);
-      view.setColumns([0, 1,
-                       { calc: "stringify",
-                         sourceColumn: 1,
-                         type: "string",
-                         role: "annotation" },
-                       2]);
-
-      var options = {
-        'width':'100%',
-        'height':'100%',
-        chartArea:{left:'7%',top:'10%',width:'85%',height:'80%'},
-        backgroundColor: 'transparent',
-        legend: {position: 'none'},
-        tooltip: {
-            trigger: "none"
-        },
-        annotations: {
-            alwaysOutside: true,
-            stemColor : 'none',
-            textStyle: {
-                fontSize: 9,
-                bold: true,
-                auraColor: 'none',
-                color: '#333'
-            }
-        },
-        hAxis: {
-            viewWindowMode: 'explicit',
-            gridlines: {
-                count: 10,
+    const config = {
+        type: 'doughnut',
+        data: data,
+        plugins: [ChartDataLabels],
+        options: {
+            responsive: true,
+            elements: {
+                arc: {
+                    borderWidth: 0
+                }
             },
-            textStyle : {
-                fontSize: 9,
-                bold: true,
-                fontWeight: "900",
-                color: "#333"
-            }
-        },
-        vAxis: {
-            textStyle : {
-                fontSize: 9,
-                bold: true,
-                fontWeight: "900",
-                color: "#333"
-            }
+            animation: {
+                duration: 0
+            },
+            responsiveAnimationDuration: 0,
+            layout: {
+                padding: function(context) {
+                    console.log(context)
+                    if(context.chart.width < 200) {
+                        return {
+                            left: 5,
+                            right: 10,
+                            top: 12,
+                            bottom: 0
+                        }                 
+                    }
+                    return {
+                        left: 20,
+                        right: 20,
+                        top: 15,
+                        bottom: 0
+                    }
+                },
+            },
+            plugins: {
+                legend: false,
+                tooltip: false,
+                datalabels: {
+                    display:  function(context) {
+                        return context.chart.chartArea.width > 100;
+                    },
+                    color: '#333333',
+                    align: function(context) {
+                        console.log(context)
+                        if (context.chart.chartArea.width < 200){
+                            return 'center';
+                        }
+                        if(context.chart.data.labels[context.dataIndex].length < 6) {
+                            return 'end';
+                        }
+                        return 'center';
+                    },
+                    anchor: 'end',
+                    backgroundColor: 'rgba(255,255,255,.7)',
+                    borderRadius: 10,
+                    textAlign: 'center',
+                    font: {
+                        size: 8,
+                        weight: 'bold',
+                    },
+                    formatter: function(value, context) {
+                        let label = context.chart.data.labels[context.dataIndex];
+                        let text = value + ' (' + Math.round((value*100)/total) + '%)'
+                        if(context.chart.chartArea.width < 200) {
+                            label = label.toUpperCase().substring(0, 3);
+                            text = Math.round((value*100)/total) + '%';
+                        }
+                        return [label.split(" "), text];
+                    }
+                },
+                doughnutlabel: {
+                    labels: [
+                        {
+                            text: total,
+                            font: {
+                                size: 20,
+                                weight: 'bold',
+                            },
+                        },
+                        {
+                            text: 'total de Focos',
+                        },
+                    ],
+                },
+            },
         }
     };
-    var chart = new google.visualization.BarChart(document.getElementById('grafico-barras'));
-    chart.draw(view, options);
+    new Chart(ctx, config);
 }
 
+function drawChartEstado() {
+    let total = 2759;
+    let el = document.getElementById('grafico-barras')
+    let ctx = el.getContext('2d');
+    Chart.defaults.font.size = 9;
+    Chart.defaults.font.weight = 'bold'
+    const data = {
+        labels: ['MT', 'RR', 'MS', 'PA', 'MA', 'RS', 'AM', 'BA', 'MG', 'RO'],
+        datasets: [{
+          label: '',
+          data: [556, 371, 205, 202, 185, 174, 157, 149, 110, 80],
+          backgroundColor: ['#df4226', '#e66650', '#e38879', '#e38879', '#e38879', '#e38879', '#e38879', '#e3aba6', '#e3aba6', '#e3aba6'],
+          hoverOffset: 4,
+        }]
+    };
+    const config = {
+        type: 'bar',
+        data: data,
+        plugins: [ChartDataLabels],
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            indexAxis: 'y',
+            animation: {
+                duration: 0
+            },
+            responsiveAnimationDuration: 0,
+            layout: {
+                padding: {
+                    left: 0,
+                    right: 0,
+                    top: 0,
+                    bottom: 0
+                }
+            },
+            plugins: {
+                legend: false,
+                tooltip: false,
+                datalabels: {
+                    color: '#333333',
+                    align: 'end',
+                    anchor: 'end',
+                    textAlign: 'center',
+                    font: {
+                        size: 8,
+                        weight: 'bold',
+                    }
+                }
+            },
+        }
+    };
+    new Chart(ctx, config);
+}
+
+function drawChartDiario() {
+    let total = 2759;
+    let el = document.getElementById('grafico-colunas')
+    let ctx = el.getContext('2d');
+    Chart.defaults.font.size = 9;
+    Chart.defaults.font.weight = 'bold'
+    const data = {
+        labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'],
+        datasets: [{
+          label: '',
+          data: [19, 66, 29, 80, 103, 69, 65, 31, 45, 37, 147, 119, 74, 76, 52, 158, 111, 153, 145, 146, 165, 350, 113, 101, 107, 41, 40, 26, 15, 50, 26],
+          backgroundColor: ['#df4226'],
+          hoverOffset: 4,
+        }]
+    };
+    const config = {
+        type: 'bar',
+        data: data,
+        plugins: [ChartDataLabels],
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            animation: {
+                duration: 0
+            },
+            responsiveAnimationDuration: 0,
+            layout: {
+                padding: {
+                    left: 0,
+                    right: 0,
+                    top: 0,
+                    bottom: 0
+                }
+            },
+            plugins: {
+                legend: false,
+                tooltip: false,
+                datalabels: {
+                    display:  function(context) {
+                        return context.chart.chartArea.width > 100;
+                    },
+                    color: '#333333',
+                    align: 'end',
+                    anchor: 'end',
+                    textAlign: 'center',
+                    font: {
+                        size: 8,
+                        weight: 'bold',
+                    }
+                }
+            },
+        }
+    };
+    new Chart(ctx, config);
+    let div = document.createElement("div");
+    var total_focos = document.createTextNode(total);
+    var label_total_focos = document.createElement("label");
+    label_total_focos.appendChild(document.createTextNode("Total de focos"));
+    div.appendChild(total_focos);
+    div.append(label_total_focos);
+    div.className = 'total-focos';
+    label_total_focos.className = 'label-total-focos';
+    document.getElementById("container-rosca").append(div);
+}
 
 function drawChart() {
-    drawColumnChart();
-    drawBarChart();
-    drawDonutChart();
+    drawChartDiario();
+    drawChartEstado();
+    drawChartBioma();
 }
